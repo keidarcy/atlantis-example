cf4b26cc61335acf678b6eb697fb2109c5814957
Xing Yahao <keidarcy.1015@gmail.com>
2023-10-12 14:55:33 +0900

    test cdktf

---
+++ cdktf-aws/main.ts
@@ -0,0 +1,30 @@
+import { Construct } from 'constructs';
+import { App, S3Backend, TerraformStack } from 'cdktf';
+import { provider } from '@cdktf/provider-aws';
+import { S3Bucket } from './.gen/providers/aws/s3-bucket';
+
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    new provider.AwsProvider(this, 'aws_default', {
+      region: 'ap-northeast-1',
+      accessKey: process.env.AWS_ACCESS_KEY,
+      secretKey: process.env.AWS_SECRET_KEY,
+    });
+
+    new S3Backend(this, {
+      bucket: 'meetsmore-dev-tfstates',
+      key: `test-cdk-tf/terraform.tfstate`,
+      region: 'ap-northeast-1',
+    });
+
+    new S3Bucket(this, 'bucket', {
+      bucket: 'demo',
+    });
+  }
+}
+
+const app = new App();
+new MyStack(app, 'cdktf-aws');
+app.synth();
